5f8cdc3c680a37cf37f5a7b4dbd991a22abe7187
NagaSaki-SoyoLin <1825679368@qq.com>
2026-08-27 23:10:29 +0800

    feat: 添加第9章网络编程和第10章多线程示例

---
+++ 第9章/09.演示单任务.py
@@ -0,0 +1,20 @@
+﻿"""
+案例: 演示单任务, 前边不执行完毕, 后边绝对无法执行
+"""
+
+
+# 1.定义函数A, 输出10次 hello world
+def func_a():
+    for i in range(10):  # 原为1000000次, 会导致刷屏假死, 改回与注释一致的10次
+        print("hello world")
+
+
+# 2. 定义函数B, 输出10次 hello python
+def func_b():
+    for i in range(2):
+        print("hello python")
+
+
+func_a()
+print('-' * 23)
+func_b()
